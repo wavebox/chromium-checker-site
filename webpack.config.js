7fe219a2c0178aef94f5921a0f55176c2fcbfa27
Thomas Beverley <tom@wavebox.io>
2021-03-10 10:28:20 +0000

    Bust the cache when serving up javascript assets

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[chunkhash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true
   },
